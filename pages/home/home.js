9a3c6b4b5ad2e5072e3e058c435c394a271eadca
Administrator <1535703141@qq.com>
2018-08-18 00:38:55 +0800

    商城首页

---
+++ pages/home/home.js
@@ -0,0 +1,127 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    classifies1:[
+      {
+        src:'tehui.png',
+        text:'今日特惠'
+      },
+      {
+        src: 'globalshop.png',
+        text: '全球购'
+      },
+      {
+        src: 'recharge.png',
+        text: '充值中心'
+      },
+      {
+        src: 'integral.png',
+        text: '我的积分'
+      }
+    ],
+    classifies2: [
+      {
+        src: 'sign.png',
+        text: '签到'
+      },
+      {
+        src: 'coupon.png',
+        text: '优惠券'
+      },
+      {
+        src: 'lightning.png',
+        text: '闪电购'
+      },
+      {
+        src: 'brand.png',
+        text: '品牌街'
+      }
+    ],
+    shopitems:[
+      {
+        src:'cooker.png',
+        text:'超值特惠电饭煲',
+        labelMoney:'99元',
+        labelLow:'超值'
+      },
+      {
+        src: 'bag.png',
+        text: '达芙妮秋款包包',
+        labelMoney: '99元',
+        labelLow: '特惠'
+      },
+      {
+        src: 'chargerbaby.png',
+        text: '易捷手机充电宝',
+        labelMoney: '49元',
+        labelLow: '推荐'
+      },
+      {
+        src: 'cup.png',
+        text: '特百惠优质水杯',
+        labelMoney: '49元',
+        labelLow: '推荐'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
